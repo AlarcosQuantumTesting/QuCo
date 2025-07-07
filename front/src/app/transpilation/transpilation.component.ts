154f0f6d7f57e6f3843971b68d59bc4a6331d8a8
Patricia <patriciadh00p@gmail.com>
2025-07-07 13:58:49 +0200

    estilo de transpilation

--- front/src/app/transpilation/transpilation.component.ts
+++ front/src/app/transpilation/transpilation.component.ts
@@ -12,7 +12,19 @@ export class TranspilationComponent {
   transpiledCode?: string;
   svgCircuit? : any
 
+  mensajeTemporal: string = '';
+  searchQuery: string = "";
+  transpilationSelected: any = null;
+  transpilationFiltered: any[] = [];
+  selectedBackend: string | null = null;
+  selectedStatusLine: any = null;
+
+
   constructor(private service: TranspilationService) { 
+    this.getTranspilationWorks();
+  }
+
+  ngOnInit() {
     this.getTranspilationWorks();
   }
 
@@ -20,6 +32,12 @@ export class TranspilationComponent {
     this.service.getListOfTranspilationWorks().subscribe({
       next: (data) => {
         this.transpilationWorks = data;
+        this.transpilationFiltered = data;
+
+        if (data.length > 0) {
+          this.transpilationSelected = data[0];
+          this.searchQuery = data[0].name;
+        }
       },
       error: (err) => {
         console.error("Error fetching transpilation works:", err);
@@ -74,5 +92,67 @@ export class TranspilationComponent {
         console.error('Error loading image', err);
       }
     });
+  }
+
+
+  onSearchInput() {
+    // Aquí normalmente no se hace nada porque el <datalist> ya lo hace
+  }
+
+  onFocusInput() {
+    
+  }
+
+  onTabPress(event: KeyboardEvent) {
+    if (event.key === 'Tab') {
+      this.selectTranspilationIfMatch();
+    }
+  }
+
+  selectTranspilationIfMatch() {
+    const match = this.transpilationWorks.find(
+      t => t.name.toLowerCase() === this.searchQuery.trim().toLowerCase()
+    );
+
+    if (match) {
+      this.transpilationSelected = match;
+      this.searchQuery = match.name;
+    }
+  }
+
+  clearSearch() {
+    this.searchQuery = '';
+    this.transpilationSelected = null;
+    this.transpilationFiltered = [...this.transpilationWorks];
+  }
+
+  searchTranspilation() {
+    const match = this.transpilationWorks.find(
+      t => t.name.toLowerCase() === this.searchQuery.trim().toLowerCase()
+    );
+
+    if (match) {
+      this.transpilationSelected = match;
+    } else {
+      console.warn('No se encontró ninguna transpilación con ese nombre.');
+    }
+  }
+
+  onBackendChange(id: Event) {
+    this.service.getTranspiledCode(id).subscribe({
+      next: (data) => {
+        this.transpiledCode = data.code;
+      },
+      error: (err) => {
+        console.error("Error fetching transpilation works:", err);
+      }
+    });
+  }
+
+
+
+  showAll() {
+    this.transpilationSelected = null;
+    this.transpilationFiltered = [...this.transpilationWorks];
   }
 }
